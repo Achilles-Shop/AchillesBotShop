0370d34f11670d5ca1ce8bfc11dd046df4f28f54
BlackKnife <sam99miraj@gmail.com>
2024-02-03 02:00:04 +0200

    1

--- js/main.js
+++ js/main.js
@@ -4,12 +4,6 @@ let hash = location.hash.substring(1);
 
 
 
-// Импорт стилей Swiper
-import 'swiper/swiper-bundle.min.css';
-
-// Импорт Swiper и его стилей
-import Swiper from 'swiper/swiper-bundle.min.js';
-
 
 // Запрос в базу данных
 const getData = async () => {
@@ -41,7 +35,54 @@ const getGoods = (callback, prop, value) => {
 
 
 // Страница категорий товаров
-// ... (ваш код до этого момента)
+try {
+  const goodsList = document.querySelector('.goods__list');
+  const goodsTitle = document.querySelector('.goods__title');
+  const changeTitle = () => {
+    goodsTitle.textContent = document.querySelector(`[href*="#${hash}"]`).textContent;
+  };
+  const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+    const li = document.createElement('li');
+    li.classList.add('goods__item');
+    li.innerHTML = `
+    <article class="good">
+      <a class="good__link-img" href="card-good.html#${id}">
+        <img class="good__img" src="./images/goods-image/${preview}" alt="">
+      </a>
+      <div class="good__description">
+        <p class="good__price">${cost} &#8381;</p>
+        <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+        ${sizes ?
+      `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` :
+      ''}
+        <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+      </div>
+    </article>
+    `;
+    return li;
+  };
+
+  const renderGoodsList = data => {
+    goodsList.textContent = '';
+    data.forEach(item => {
+      const card = createCard(item);
+      goodsList.append(card);
+    });
+  };
+
+  // Изменение содержимого при смене страниц
+  window.addEventListener('hashchange', () => {
+    hash = location.hash.substring(1);
+    getGoods(renderGoodsList, 'category', hash);
+    changeTitle();
+  });
+
+  getGoods(renderGoodsList, 'category', hash);
+  changeTitle();
+}
+catch (err) {
+  console.warn(err);
+}
 
 // Страница товара
 try {
@@ -51,43 +92,39 @@ try {
   const cardGoodPrice = document.querySelector('.card-good__price');
   const cardGoodDescr = document.querySelector('.card-good__descr');
   const cardGoodColor = document.querySelector('.card-good__color');
-  const cardGoodSelectWrapper = document.querySelector('.card-good__select-wrapper'); // Изменено на querySelector
+  const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper');
   const cardGoodColorList = document.querySelector('.card-good__color-list');
   const cardGoodSizes = document.querySelector('.card-good__sizes');
   const cardGoodSizesList = document.querySelector('.card-good__sizes-list');
   const cardGoodBuy = document.querySelector('.card-good__buy');
 
   const generateList = data => data.reduce((html, item, i) =>
-    html + `<li class="card-good__select-item" data-id="${i}">${item}</li>`, '');
+  html + `<li class="card-good__select-item" data-id="${i}">${item}</li>`, '');
 
-  const renderCardGood = ([{ id, brand, name, cost, descr, color, sizes, photos }]) => {
+  const renderCardGood = ([{ id, brand, name, cost, descr, color, sizes, photo }]) => {
     const data = { brand, name, cost, descr, id };
-    cardGoodImage.src = `./images/goods-image/${photos[0]}`;
+    cardGoodImage.src = `./images/goods-image/${photo}`;
     cardGoodImage.alt = `${brand} ${name}`;
     cardGoodBrand.textContent = brand;
     cardGoodTitle.textContent = name;
     cardGoodPrice.textContent = `${cost} ₽`;
     cardGoodDescr.textContent = descr;
-
-    // Исправлено: cardGoodImage.innerHTML = cardGoodImages;
-    cardGoodImage.innerHTML = photos.map(photo => `<div class="swiper-slide"><img src="./images/goods-image/${photo}" alt="${brand} ${name}"></div>`).join('');
-
     if (color) {
       cardGoodColor.textContent = color[0];
       cardGoodColor.dataset.id = 0;
       cardGoodColorList.innerHTML = generateList(color);
-    } else {
+    }
+    else {
       cardGoodColor.style.display = 'none';
     }
     if (sizes) {
       cardGoodSizes.textContent = sizes[0];
       cardGoodSizes.dataset.id = 0;
       cardGoodSizesList.innerHTML = generateList(sizes);
-    } else {
+    }
+    else{
       cardGoodSizes.style.display = 'none';
     }
-
-    // Исправлено: убедитесь, что getLocalStorage, setLocalStorage и deleteItemCart определены где-то в вашем коде
 
     if (getLocalStorage().some(item => item.id === id)) {
       cardGoodBuy.classList.add('delete');
@@ -103,44 +140,38 @@ try {
       }
       if (color) {
         data.color = cardGoodColor.textContent;
-      }
+      } 
       if (sizes) {
         data.size = cardGoodSizes.textContent;
       }
 
       cardGoodBuy.classList.add('delete');
       cardGoodBuy.textContent = 'Удалить из корзины';
-
+      
       const cardData = getLocalStorage();
       cardData.push(data);
       setLocalStorage(cardData);
+
     });
   };
 
-  cardGoodSelectWrapper.addEventListener('click', e => { // Изменено на addEventListener
-    const target = e.target;
-    if (target.closest('.card-good__select')) {
-      target.classList.toggle('card-good__select__open');
-    }
-    if (target.closest('.card-good__select-item')) {
-      const cardGoodSelect = cardGoodSelectWrapper.querySelector('.card-good__select'); // Изменено на cardGoodSelectWrapper
-      cardGoodSelect.textContent = target.textContent;
-      cardGoodSelect.dataset.id = target.dataset.id;
-      cardGoodSelect.classList.remove('card-good__select__open');
-    }
+  cardGoodSelectWrapper.forEach(item => {
+    item.addEventListener('click', e => {
+      const target = e.target;
+      if (target.closest('.card-good__select')) {
+        target.classList.toggle('card-good__select__open');
+      }
+      if (target.closest('.card-good__select-item')) {
+        const cardGoodSelect = item.querySelector('.card-good__select');
+        cardGoodSelect.textContent = target.textContent;
+        cardGoodSelect.dataset.id = target.dataset.id;
+        cardGoodSelect.classList.remove('card-good__select__open');
+      }
+    });
   });
 
   getGoods(renderCardGood, 'id', hash);
-} catch (err) {
+}
+catch (err) {
   console.warn(err);
 }
-
-// Инициализация Swiper
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
